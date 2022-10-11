a70ac02d231dec4ea1fcecbdd20d00aa3fd90f39
gkotecki <gabrielkotecki@gmail.com>
2022-10-11 19:00:12 -0300

    ADD: component animation and winner style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { BoardButtonComponent } from './board-button/board-button.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 import { RangeInputComponent } from './range-input/range-input.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { RangeInputComponent } from './range-input/range-input.component';
     ScoreboardComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
--- src/app/board-button/board-button.component.ts
+++ src/app/board-button/board-button.component.ts
@@ -1,16 +1,35 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 import { GameOption } from 'src/shared/services/game.service';
+
+const animation = {
+  shrink: { 'max-width': 0, 'max-height': 0, opacity: 0 },
+  default: { 'max-width': 'var(--btn-size)', 'max-height': 'var(--btn-size)', opacity: 1 },
+};
 
 @Component({
   selector: 'board-button',
   templateUrl: './board-button.component.html',
   styleUrls: ['./board-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [style(animation.shrink), animate('0.2s', style(animation.default))]),
+      transition(':leave', [style(animation.default), animate('0.2s', style(animation.shrink))]),
+    ]),
+  ],
 })
 export class BoardButtonComponent {
   @Input()
   @HostBinding('class')
   public type: GameOption = null;
+
+  @Input()
+  @HostBinding('class.winner')
+  public winner = false;
+
+  @HostBinding('@inOutAnimation')
+  slideIn = true;
 
   get imgSrc() {
     return `assets/icon-${this.type}.svg`;
